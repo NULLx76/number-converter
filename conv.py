d1ced150d03f514138cd41e99a09bd237773654d
victorheld <victor@xirion.net>
2015-11-10 20:49:35 +0100

    base

--- conv.py
+++ conv.py
@@ -9,6 +9,7 @@ menu = {
 
 while True:
     options = menu.keys()
+    options.sort()
 
     print("Convert From?")
     for entry in options:
@@ -21,6 +22,7 @@ while True:
             '2': "Decimal"
         }
         optionsB = menuB.keys()
+        optionsB.sort()
 
         print("Convert To:")
         for entryB in optionsB:
@@ -29,12 +31,14 @@ while True:
         selectionB = raw_input("Please Select:")
 
         if selectionB == "1":
+            # Binary to hex
             selectionBH = raw_input("Enter your number:")
-            print(hex(int(selectionBH, 2)))
+            print hex(int(selectionBH, 2))[2:]
             break
         elif selectionB == "2":
+            # Binary to decimal
             selectionBD = raw_input("Enter your number:")
-            print(int(selectionBD, 2))
+            print int(selectionBD, 2)
             break
         else:
             print("Not a valid choice, please try again")
@@ -45,6 +49,7 @@ while True:
             '2': "Decimal"
         }
         optionsH = menuH.keys()
+        optionsH.sort()
 
         print("Convert To:")
         for entryH in optionsH:
@@ -55,16 +60,42 @@ while True:
         if selectionH == "1":
             selectionHB = raw_input("Enter your number:")
             # Hex to binary
+            print bin(int(selectionHB, 16))[2:]
             break
         elif selectionH == "2":
             selectionHD = raw_input("Enter your number:")
             # hex to decimal
+            print int(selectionHD, 16)
             break
         else:
             print("Not a valid choice, please try again")
 
     elif selection == '3':
-        print "find"
+        menuD = {
+            '1': "Binary",
+            '2': "Hex"
+        }
+        optionsD = menuD.keys()
+
+        print("Convert To:")
+        for entryD in optionsD:
+            print entryD, menuD[entryD]
+
+        selectionD = raw_input("Please Select:")
+
+        if selectionD == "1":
+            selectionD = raw_input("Enter your number:")
+            # Decimal to binary
+            print bin(int(selectionD, 10))[2:]
+            break
+        elif selectionD == "2":
+            selectionDH = raw_input("Enter your number:")
+            # Decimal to hex
+            print hex(int(selectionDH, 10))[2:]
+            break
+        else:
+            print("Not a valid choice, please try again")
+
     elif selection == '4':
         break
     else:
